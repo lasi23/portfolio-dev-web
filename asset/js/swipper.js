e14127ebcfa3393ca39761023c0c9766d8b2e088
lasi23 <lsilvere39@gmail.com>
2026-03-13 18:27:05 +0100

    ajout de boutton stop and start sur l'annimation swipper

--- asset/js/swipper.js
+++ asset/js/swipper.js
@@ -7,9 +7,17 @@ let swiper = new Swiper(".swiper", {
     delay: 1000,
     disableOnInteraction: false
   },
-  ShadowRoot: true,
 
   cardsEffect: {
     slideShadows: true
   }
+});
+
+let stopButton = document.getElementById("stopButton");
+
+stopButton.addEventListener("click", () => {
+  swiper.autoplay.stop();
+});
+startButton.addEventListener("click", () => {
+  swiper.autoplay.start();
 });
