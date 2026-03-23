bcbbe7ca6607903d45a26cd8aa4d0ff96f60feb5
lasi23 <lsilvere39@gmail.com>
2026-03-23 17:29:56 +0100

    fix: encapsuler l'animation du slogan dans une condition de largeur d'écran

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,18 @@
-// Séparer le texte en lettres
-const titre = document.getElementById('anim-slogan');
-titre.innerHTML = titre.textContent
-    .split('')
-    .map(char => char === ' ' ? `<span class="char">&nbsp;</span>` : `<span class="char">${char}</span>`)
-    .join('');
+if (window.innerWidth > 768) {
+    const titre = document.getElementById('anim-slogan');
 
-// Animation des lettres
-anime({
-    targets: '.char',
-    translateY: [-50, 0],   // départ de 50px au-dessus
-    rotate: [-360, 0],      // rotation d'un tour complet
-    duration: 800,
-    delay: anime.stagger(50), // décalage entre chaque lettre
-    easing: 'easeOutBounce',
-    loop: false
-});
+    titre.innerHTML = titre.textContent
+        .split('')
+        .map(char => char === ' ' ? `<span class="char">&nbsp;</span>` : `<span class="char">${char}</span>`)
+        .join('');
+
+    anime({
+        targets: '.char',
+        translateY: [-50, 0],
+        rotate: [-360, 0],
+        duration: 800,
+        delay: anime.stagger(100),
+        easing: 'easeOutBounce',
+        loop: true
+    });
+}
